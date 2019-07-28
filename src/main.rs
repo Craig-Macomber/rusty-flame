bf85bcd11c75fc101dd7070d4851bcaa8a9e5ca9
Craig <CraigM@CraigM.info>
2019-07-27 18:44:07 -0700

    make process_scene more adjustable

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate piston;
 
 use crate::flame::process_levels;
 use crate::flame::AffineState;
-use na::{Affine2, Rotation2, Similarity2, Translation2};
+use na::{Affine2, Point2, Rotation2, Similarity2, Translation2};
 
 mod flame;
 mod piston_render;
@@ -23,21 +23,26 @@ pub fn process_scene<F: FnMut(&AffineState)>(
     draw_size: [f64; 2],
     callback: &mut F,
 ) {
-    let sm = Similarity2::from_scaling(0.5);
+    let n: u32 = 3;
+    let shift = 0.5;
+    let scale = 0.5;
+    let sm = Similarity2::from_scaling(scale);
 
-    let a: Affine2<f64> = na::convert(sm * Translation2::new(0.25, 0.25));
-    let a2: Affine2<f64> = na::convert(sm * Translation2::new(-0.25, 0.25));
+    let mut va = (0..n)
+        .map(|i| {
+            let offset = Rotation2::new(std::f64::consts::PI * 2.0 * f64::from(i) / f64::from(n))
+                * Point2::new(shift, 0.0);
+            na::convert(sm * Translation2::new(offset.x, offset.y))
+        })
+        .collect::<Vec<Affine2<f64>>>();
 
-    let transforms = vec![
-        na::convert(sm * Translation2::new(0.0, -0.25)),
-        a2 * Rotation2::new(100.0 * (cursor[1] as f64) / draw_size[1]),
-        a * Rotation2::new(100.0 * (cursor[0] as f64) / draw_size[0]),
-    ];
+    va[0] *= Rotation2::new(20.0 * (cursor[1] as f64) / draw_size[1]);
+    va[1] *= Rotation2::new(20.0 * (cursor[0] as f64) / draw_size[0]);
 
     let mat_root = na::convert(
         Translation2::new(cursor[0], cursor[1]) * Similarity2::from_scaling(draw_size[0] as f64),
     );
 
-    let state = AffineState::new(mat_root, &transforms);
+    let state = AffineState::new(mat_root, &va);
     process_levels(8, &state, callback);
 }
