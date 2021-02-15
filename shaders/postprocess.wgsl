ce22551f4f26b68bbdec0e1fb13625c31b6a588d
Craig <CraigM@CraigM.info>
2021-02-14 19:37:47 -0800

    use gradient image

--- shaders/postprocess.wgsl
+++ shaders/postprocess.wgsl
@@ -25,22 +25,17 @@ var r_color: texture_2d<f32>;
 [[group(0), binding(1)]]
 var r_sampler: sampler;
 
+[[group(1), binding(0)]]
+var r_gradient: texture_1d<f32>;
+[[group(1), binding(1)]]
+var r_gradient_sampler: sampler;
+
 [[stage(fragment)]]
 fn fs_main() {
     // TODO: non-filtered interger sampler?
     var tex: vec4<f32> = textureSample(r_color, r_sampler, in_tex_coord_fs);
     var v: f32 = tex.x;
-    var l: f32 = log2(v) / 3.0;
+    var l: f32 = log2(v);
 
-    l = l % 9.0;
-
-    if ( l > 6.0) {
-        l = l - 6.0;
-        out_color = vec4<f32>(l, l - 1.0, l - 2.0, 1.0);
-    } elseif ( l > 3.0) {
-        l = l - 3.0;
-        out_color = vec4<f32>(1.0 - l, 2.0 - l, 3.0 - l, 1.0);
-    } else {
-        out_color = vec4<f32>(l, l - 1.0, l - 2.0, 1.0);
-    }
+    out_color = textureSample(r_gradient, r_gradient_sampler, l / 100.0);
 }
--- src/wgpu_render.rs
+++ src/wgpu_render.rs
@@ -314,9 +314,106 @@ pub async fn run(event_loop: EventLoop<()>, window: Window) {
 
     let large_bind_group = make_bind_group(&last_pass, false);
 
+    let diffuse_bytes = include_bytes!("../images/gradient.png");
+    let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+    let diffuse_rgba = diffuse_image.as_rgba8().unwrap();
+
+    use image::GenericImageView;
+    let dimensions = diffuse_image.dimensions();
+
+    let texture_size = wgpu::Extent3d {
+        width: dimensions.0,
+        height: dimensions.1,
+        depth: 1,
+    };
+
+    let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
+        // All textures are stored as 3D, we represent our 2D texture
+        // by setting depth to 1.
+        size: texture_size,
+        mip_level_count: 1, // We'll talk about this a little later
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D1,
+        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+        // SAMPLED tells wgpu that we want to use this texture in shaders
+        // COPY_DST means that we want to copy data to this texture
+        usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+        label: Some("diffuse_texture"),
+    });
+
+    queue.write_texture(
+        // Tells wgpu where to copy the pixel data
+        wgpu::TextureCopyView {
+            texture: &diffuse_texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+        },
+        // The actual pixel data
+        diffuse_rgba,
+        // The layout of the texture
+        wgpu::TextureDataLayout {
+            offset: 0,
+            bytes_per_row: 4 * dimensions.0,
+            rows_per_image: dimensions.1,
+        },
+        texture_size,
+    );
+
+    let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+    let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+        address_mode_u: wgpu::AddressMode::ClampToEdge,
+        address_mode_v: wgpu::AddressMode::ClampToEdge,
+        address_mode_w: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Linear,
+        min_filter: wgpu::FilterMode::Nearest,
+        mipmap_filter: wgpu::FilterMode::Nearest,
+        ..Default::default()
+    });
+
+    let gradient_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        multisampled: false,
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D1,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStage::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler {
+                        comparison: false,
+                        filtering: true,
+                    },
+                    count: None,
+                },
+            ],
+            label: None,
+        });
+
+    let gradient_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &gradient_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+            },
+        ],
+        label: None,
+    });
+
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: Some("postprocess pipeline"),
-        bind_group_layouts: &[&accumulation_bind_group_layout],
+        bind_group_layouts: &[&accumulation_bind_group_layout, &gradient_bind_group_layout],
         push_constant_ranges: &[],
     });
 
@@ -480,6 +577,7 @@ pub async fn run(event_loop: EventLoop<()>, window: Window) {
 
                             postprocess_pass.set_pipeline(&postprocess_pipeline);
                             postprocess_pass.set_bind_group(0, &large_bind_group, &[]);
+                            postprocess_pass.set_bind_group(1, &gradient_bind_group, &[]);
                             postprocess_pass.set_vertex_buffer(0, quad_buf.slice(..));
                             postprocess_pass.draw(0..(quad_vertexes.len() as u32), 0..1);
                         }
